5227faf8516e2534718381d75f03c90af8c2d501
A Zakharov <pro@0x3a.xyz>
2026-01-25 14:46:21 +0300

    issue-15 - добавлены поля дата и исполнитель в форму результата производства

--- src/app/components/guard-area/manufacturing/manufacturing-success/manufacturing-success.ts
+++ src/app/components/guard-area/manufacturing/manufacturing-success/manufacturing-success.ts
@@ -3,7 +3,8 @@ import {injectContext} from '@taiga-ui/polymorpheus';
 import {TuiDialogContext} from '@taiga-ui/core';
 import {UsedLot} from '../../../../services/manufacturing/combination';
 import {TuiTableDirective, TuiTableTbody, TuiTableTh, TuiTableThGroup, TuiTableTr} from '@taiga-ui/addon-table';
-import {NgFor} from '@angular/common';
+import {AsyncPipe, DatePipe, NgFor} from '@angular/common';
+import {ExecutorPipe} from '../../../../pipes/executor-pipe';
 
 @Component({
   selector: 'app-manufacturing-success',
@@ -14,17 +15,26 @@ import {NgFor} from '@angular/common';
     TuiTableThGroup,
     TuiTableTr,
     NgFor,
+    DatePipe,
+    ExecutorPipe,
+    AsyncPipe,
   ],
   templateUrl: './manufacturing-success.html',
   styleUrl: './manufacturing-success.scss',
 })
 export class ManufacturingSuccess {
 
-  public readonly context = injectContext<TuiDialogContext<void, UsedLot[]>>();
+  public readonly context = injectContext<TuiDialogContext<void, ManufacturingSuccessOptions>>();
 
   protected columns = ['name', 'lot', 'originalTaken'];
 
   protected get data() {
     return this.context.data;
   }
+}
+
+export interface ManufacturingSuccessOptions {
+  usedLots: UsedLot[];
+  executorId: string;
+  date: Date;
 }
