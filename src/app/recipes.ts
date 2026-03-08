7e1c162f5aa4c93eb4ed6982941e0162fb4de1d4
A Zakharov <pro@0x3a.xyz>
2026-03-08 17:01:42 +0300

    quarantine - исправлены коды в рецептах

--- src/app/recipes.ts
+++ src/app/recipes.ts
@@ -4,30 +4,30 @@ import {PositionType} from './services/collections/positions.collection';
 export const chipRecipe: Recipe = {
   code: 'chip',
   items: [
-    {type: PositionType.Checked, code: 'body', quantity: 1},
-    {type: PositionType.Checked, code: 'membrane', quantity: 1},
-    {type: PositionType.Checked, code: 'bottom_lid', quantity: 1},
-    {type: PositionType.Checked, code: 'top_lid', quantity: 1},
-    {type: PositionType.Checked, code: 'tape_3M', quantity: 1},
+    {type: PositionType.Checked, code: 'КО', quantity: 1},
+    {type: PositionType.Checked, code: 'МБ', quantity: 1},
+    {type: PositionType.Checked, code: 'НК', quantity: 1},
+    {type: PositionType.Checked, code: 'ВК', quantity: 1},
+    {type: PositionType.Checked, code: 'КЛ', quantity: 1},
   ],
 };
 export const packRecipe: Recipe = {
   code: 'chip_pack',
   items: [
     {type: PositionType.Produced, code: 'chip', quantity: 5},
-    {type: PositionType.Checked, code: 'bag_vinar', quantity: 5},
-    {type: PositionType.Normal, code: 'label_58х40', quantity: 6},
-    {type: PositionType.Normal, code: 'box_unit', quantity: 1},
-    {type: PositionType.Normal, code: 'manual', quantity: 1},
-    {type: PositionType.Normal, code: 'label_seal', quantity: 1},
+    {type: PositionType.Checked, code: 'ПК', quantity: 5},
+    {type: PositionType.Normal, code: 'ЭУ', quantity: 6},
+    {type: PositionType.Normal, code: 'КУ', quantity: 1},
+    {type: PositionType.Normal, code: 'ИП', quantity: 1},
+    {type: PositionType.Normal, code: 'ЭП', quantity: 1},
   ],
 };
 export const shipRecipe: Recipe = {
   code: 'chip_ship',
   items: [
     {type: PositionType.Produced, code: 'chip_pack', quantity: 10},
-    {type: PositionType.Normal, code: 'label_ship', quantity: 1},
-    {type: PositionType.Normal, code: 'box_ship', quantity: 1},
+    {type: PositionType.Normal, code: 'ЭП', quantity: 1},
+    {type: PositionType.Normal, code: 'КОТ', quantity: 1},
   ],
   extraFields: {
     recipient: true,
